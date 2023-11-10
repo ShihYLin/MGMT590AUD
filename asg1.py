25f79b3b3e1ca2cc90ebce73b5509e3dfcf55273
ShihYLin <111937472+ShihYLin@users.noreply.github.com>
2023-11-10 13:17:40 -0500

    lemmatized

--- asg1.py
+++ asg1.py
@@ -3,8 +3,22 @@ import numpy as np
 import sklearn
 import nltk
 
-df = pd.read_csv("/Users/ziyun/Documents/MGMT 590 AUD/Assignment 1.csv")
+#import csv file
+df = pd.read_csv("/Users/ziyun/Documents/MGMT590AUD/Assignment 1.csv")
 
+#tokenize and lowercase
 df['tokenized'] = df.iloc[:,1].apply(lambda x: nltk.word_tokenize(x))
 
-print(df.head())
+print(df['tokenized'].head())
+
+# Lemmatize the 'tokenized' column in each row and remove punctuation
+lemmatizer = nltk.stem.WordNetLemmatizer()
+lemmatized_tokens = []
+
+for row in df['tokenized']:
+    lemmatized_row = [lemmatizer.lemmatize(token) for token in row if token.isalpha()]
+    lemmatized_tokens.append(lemmatized_row)
+
+# Print the lemmatized tokens for each row
+for tokens in lemmatized_tokens:
+    print(tokens)
