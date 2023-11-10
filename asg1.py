e8c8332db4b09499d91f4e7533098b5ee22764e3
ShihYLin <111937472+ShihYLin@users.noreply.github.com>
2023-11-10 17:54:33 -0500

    remove stop words and punctuation

--- asg1.py
+++ asg1.py
@@ -22,3 +22,16 @@ for row in df['tokenized']:
 # Print the lemmatized tokens for each row
 for tokens in lemmatized_tokens:
     print(tokens)
+    
+# Remove stop words and punctuation
+from nltk.corpus import stopwords
+def remove_stopwords(word_list):
+    stop_words = set(stopwords.words('english'))
+    filtered_words = [word for word in word_list if word.lower() not in stop_words and word.isalpha()]
+    return filtered_words
+
+filtered_list_of_lists = [remove_stopwords(word_list) for word_list in lemmatized_tokens]
+# Print the original and filtered lists
+print("\nList of Lists without Stopwords:")
+for word_list in filtered_list_of_lists:
+    print(word_list)
