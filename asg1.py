a763e8a5032c63aa890684767b9a5032672f1704
ShihYLin <111937472+ShihYLin@users.noreply.github.com>
2023-11-11 09:23:44 -0500

    TF-IDF

--- asg1.py
+++ asg1.py
@@ -45,5 +45,12 @@ flat_list = [word for sublist in filtered_list_of_lists for word in sublist]
 vectorizer1 = CountVectorizer(min_df=3)
 vectorizer1.fit(flat_list)
 print(vectorizer1.vocabulary_)
-v1 = vectorizer1.transform(flat_list)
-print(v1.toarray())
+
+#TF_IDF
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer2 = TfidfVectorizer()
+vectorizer2.fit(flat_list)
+print(vectorizer2.vocabulary_)
+
+v2 = vectorizer2.transform(flat_list)
+print(v2.toarray())
