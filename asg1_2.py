52e0925f6b4bfef9d0c24ea3654d2abe95867227
ShihYLin <111937472+ShihYLin@users.noreply.github.com>
2023-11-12 11:00:00 -0500

    get first ten rows

--- asg1_2.py
+++ asg1_2.py
@@ -7,4 +7,6 @@ df = pd.read_csv("/Users/ziyun/Documents/MGMT590AUD/Assignment 1.csv")
 # Tokenize step 1
 df['tokenized'] = df.iloc[:,1].apply(lambda x: nltk.word_tokenize(x))
 
-print(df['tokenized'].head())
+# Get first ten rows
+first_ten_rows = df['tokenized'].head(10)
+print(first_ten_rows)
