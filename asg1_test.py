b7ca7f45805901c6ad3b8f5da8aaa4156341956e
ShihYLin <111937472+ShihYLin@users.noreply.github.com>
2023-11-12 16:18:58 -0500

    save txt and print dimension

--- asg1_test.py
+++ asg1_test.py
@@ -9,12 +9,23 @@ df = pd.read_csv("/Users/ziyun/Documents/MGMT590AUD/Assignment 1.csv")
 df['tokenized'] = df.iloc[:, 1].apply(lambda x: nltk.word_tokenize(x))
 
 # Get first ten rows after tokenization
-first_ten_rows = df['tokenized'].head(10)
+first_ten_rows = df['tokenized'][:10]
 print(first_ten_rows)
 
 # Use LabelEncoder for encoding the text data
 label_encoder = LabelEncoder()
 df['encoded_text'] = first_ten_rows.apply(lambda x: label_encoder.fit_transform(x))
 
-# Save the encoded text to a text file
-df['encoded_text'].to_csv("/Users/ziyun/Documents/MGMT590AUD/encoded_text.txt", header=False, index=False)
+# Specify the text file path
+txt_file_path = '/Users/ziyun/Documents/MGMT590AUD/encoded_text_output.txt'
+
+# Open the text file in write mode
+with open(txt_file_path, 'w') as txtfile:
+    # Write each encoded row to a new line in the text file
+    for encoded_row in df['encoded_text'][:10]:
+        txtfile.write(str(encoded_row) + '\n')
+
+print(f"The encoded text has been saved to {txt_file_path}")
+
+# Print the output dimension
+print("Output Dimension:", df['encoded_text'][:10].shape)
