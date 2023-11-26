0955e8a473fbdb8ba2d7bd8fd8ff6534f20df78f
ShihYLin <111937472+ShihYLin@users.noreply.github.com>
2023-11-25 19:26:30 -0500

    add file and resize

---
+++ asg2.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import os
+
+# Path to the folder containing images
+folder_path = '/Users/ziyun/Documents/MGMT590AUD/image'
+
+# Create a new folder to save resized images
+output_folder = '/Users/ziyun/Documents/MGMT590AUD/resized_images'
+os.makedirs(output_folder, exist_ok=True)
+
+# Iterate through all files in the folder
+for filename in os.listdir(folder_path):
+    try:
+        # Open the image file
+        img_path = os.path.join(folder_path, filename)
+        img = Image.open(img_path)
+
+        # Resize the image to 100x100 pixels
+        resized_img = img.resize((100, 100))
+
+        # Save the resized image to the output folder
+        output_path = os.path.join(output_folder, filename)
+        resized_img.save(output_path)
+
+        print(f"Resized and saved {filename} successfully.")
+    except Exception as e:
+        print(f"Error processing {filename}: {e}")
